d487edc004213642c70128a601d8d99118b8a353
pablo perez <pablopgalaz80@gmail.com>
2023-09-04 16:26:35 -0300

    delete log.

--- cqrs-user-command-service/src/main/java/dev/amir/usercommand/UserCommandServiceApplication.java
+++ cqrs-user-command-service/src/main/java/dev/amir/usercommand/UserCommandServiceApplication.java
@@ -1,15 +1,12 @@
 package dev.amir.usercommand;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class UserCommandServiceApplication {
 
     public static void main(String[] args) {
-        log.info("Starting User Command Service application.");
         SpringApplication.run(UserCommandServiceApplication.class, args);
     }
 }
