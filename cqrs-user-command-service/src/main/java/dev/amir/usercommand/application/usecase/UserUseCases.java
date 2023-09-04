f91077c1c3d26f3cf53e585878fd510ab2360d1a
pablo perez <pablopgalaz80@gmail.com>
2023-09-04 16:18:05 -0300

    Add correction and changes log command-aplication.

--- cqrs-user-command-service/src/main/java/dev/amir/usercommand/application/usecase/UserUseCases.java
+++ cqrs-user-command-service/src/main/java/dev/amir/usercommand/application/usecase/UserUseCases.java
@@ -5,9 +5,11 @@ import dev.amir.usercommand.application.port.output.UserMessageOutputPort;
 import dev.amir.usercommand.application.port.output.UserOutputPort;
 import dev.amir.usercommand.domain.entity.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserUseCases implements UserInputPort {
@@ -17,31 +19,37 @@ public class UserUseCases implements UserInputPort {
 
     @Override
     public String createUser(User user) {
+        log.info("Verifying if the 'id' field is empty for user creation.");
         if (StringUtils.hasText(user.getId())) {
             throw new IllegalArgumentException("Invalid User, id field must be empty");
         }
 
         User savedUser = userOutputPort.save(user);
+        log.info("User successfully created and saved with id: " + savedUser.getId());
         userMessageOutputPort.sendMessage(savedUser);
         return savedUser.getId();
     }
 
     @Override
     public void updateUser(User user) {
+        log.info("Verifying if the 'id' field exists for user update.");
         if (!StringUtils.hasText(user.getId())) {
             throw new IllegalArgumentException("Invalid User, id field must exist");
         }
 
         User savedUser = userOutputPort.save(user);
+        log.info("User successfully created/updated with id: " + savedUser.getId());
         userMessageOutputPort.sendMessage(savedUser);
     }
 
     @Override
     public boolean deleteUser(String userId) {
+        log.info("Attempting to delete user with ID: " + userId);
         if (!StringUtils.hasText(userId)) {
             throw new IllegalArgumentException("Invalid User, id field must exist");
         }
-
+        
+        log.info("Attempting to delete user with ID: " + userId);
         return userOutputPort.delete(userId);
     }
 }
