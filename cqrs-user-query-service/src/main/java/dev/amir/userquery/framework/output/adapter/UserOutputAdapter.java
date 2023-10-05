aa4dd73e93fcbadcb034e9dd705dbcd9316b843d
pilo1987 <140113440+pilo1987@users.noreply.github.com>
2023-10-05 11:23:00 -0300

    AB#17 Add logs in framework layer of User-Query-Service (#14)
    Co-authored-by: pablo perez <pablopgalaz80@gmail.com>
    Co-authored-by: Amir Aranibar <amir.aranibar@gmail.com>

--- cqrs-user-query-service/src/main/java/dev/amir/userquery/framework/output/adapter/UserOutputAdapter.java
+++ cqrs-user-query-service/src/main/java/dev/amir/userquery/framework/output/adapter/UserOutputAdapter.java
@@ -9,8 +9,10 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserOutputAdapter implements UserOutputPort {
@@ -19,20 +21,24 @@ public class UserOutputAdapter implements UserOutputPort {
 
     @Override
     public Collection<User> getAll() {
+        log.info("Getting all users from the database");
         return userMongoRepository.findAll()
-            .stream()
-            .map(userMongoMapper::convert)
-            .collect(Collectors.toList());
+                .stream()
+                .map(userMongoMapper::convert)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<User> getById(String userId) {
+        log.info("Getting user by ID {} from the database", userId);
         return userMongoRepository.findById(userId).map(userMongoMapper::convert);
     }
 
     @Override
     public User save(User user) {
+        log.info("Saving user with ID {} in the database", user.getId());
         UserMongo savedUser = userMongoRepository.save(userMongoMapper.convert(user));
+        log.info("User with ID {} saved in the database", savedUser.getId());
         return userMongoMapper.convert(savedUser);
     }
 }
