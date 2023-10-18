f7b6f69b29980addae11165b08d40c9f33867a64
Kevin Garcia <huascraft@hotmail.com>
2023-10-18 15:51:25 -0400

    add exception handler logs

--- cqrs-user-command-service/src/main/java/dev/amir/usercommand/framework/input/rest/handler/UserResponseExceptionHandler.java
+++ cqrs-user-command-service/src/main/java/dev/amir/usercommand/framework/input/rest/handler/UserResponseExceptionHandler.java
@@ -1,21 +1,25 @@
 package dev.amir.usercommand.framework.input.rest.handler;
 
 import dev.amir.usercommand.domain.exception.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class UserResponseExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnknownException(Exception ex) {
+        log.error("An unexpected error occurred", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Internal error");
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        log.error("The user was not found", ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body("User not found");
     }
