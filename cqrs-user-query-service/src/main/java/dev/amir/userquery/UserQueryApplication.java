e5153a4fbafe9d9203c143fa05a6d4909c745600
Amir Aranibar <amir.aranibar@gmail.com>
2023-10-18 19:36:01 +0200

    Remove no-args constructor test

--- cqrs-user-query-service/src/main/java/dev/amir/userquery/UserQueryApplication.java
+++ cqrs-user-query-service/src/main/java/dev/amir/userquery/UserQueryApplication.java
@@ -1,9 +1,11 @@
 package dev.amir.userquery;
 
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NoArgsConstructor
 public class UserQueryApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserQueryApplication.class, args);
--- cqrs-user-query-service/src/test/java/dev/amir/userquery/UserQueryApplicationTest.java
+++ cqrs-user-query-service/src/test/java/dev/amir/userquery/UserQueryApplicationTest.java
@@ -1,11 +1,9 @@
 package dev.amir.userquery;
 
-import java.lang.reflect.InvocationTargetException;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.springframework.boot.SpringApplication;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mockStatic;
@@ -25,15 +23,5 @@ class UserQueryApplicationTest {
                     () -> SpringApplication.run(eq(UserQueryApplication.class), eq(arg1))
             );
         }
-    }
-
-    @Test
-    void test_NoArgsConstructor() throws
-            NoSuchMethodException,
-            InvocationTargetException,
-            InstantiationException,
-            IllegalAccessException {
-        UserQueryApplication app = UserQueryApplication.class.getDeclaredConstructor().newInstance();
-        assertNotNull(app);
     }
 }
