ada38936deb90727534e62801efe9967ca714c21
Amir Aranibar <amir.aranibar@gmail.com>
2023-10-29 19:53:26 +0100

    Fix DB script error after first running

--- cqrs-user-command-service/docker-config/init-db.sh
+++ cqrs-user-command-service/docker-config/init-db.sh
@@ -7,4 +7,4 @@ sleep 15
 # Create the database if it doesn't exist
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P ${MSSQL_SA_PASSWORD} -Q "CREATE DATABASE [${MSSQL_DB_NAME}]"
 # Keep the container running
-tail -f /dev/null
+sleep infinity
