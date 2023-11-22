e010cd461f56693587c35e466b236e9fdc8349d2
Kevin Garcia <huascraft@hotmail.com>
2023-11-22 14:00:44 -0400

    delete saveMessage

--- cqrs-user-query-service/src/main/java/dev/amir/userquery/framework/input/rabbitmq/message/SaveUserMessage.java
+++
@@ -1,8 +0,0 @@
-package dev.amir.userquery.framework.input.rabbitmq.message;
-
-import dev.amir.userquery.domain.valueobject.UserGender;
-import dev.amir.userquery.domain.valueobject.UserStatus;
-
-public record SaveUserMessage(String id, String name, String lastname, String email, UserStatus status,
-                              UserGender gender) {
-}
