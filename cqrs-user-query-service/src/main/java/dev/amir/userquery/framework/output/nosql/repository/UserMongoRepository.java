8387828458a24f7c33c5a9dd5b7baa5b52d7eaae
Alan <104171943+huascraft@users.noreply.github.com>
2023-11-29 10:15:26 -0400

    AB#233 Remove rabbit mq implementation (#49)

--- cqrs-user-query-service/src/main/java/dev/amir/userquery/framework/output/nosql/repository/UserMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.amir.userquery.framework.output.nosql.repository;
-
-import dev.amir.userquery.framework.output.nosql.entity.UserMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<UserMongo, String> {
-}
