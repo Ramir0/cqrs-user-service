ac7655f3bad92f12e796360718508f2e49752e4a
Amir Aranibar <amir.aranibar@gmail.com>
2023-12-02 15:24:26 +0100

    Remove H2 and MongoDB from integration tests in User Query

--- cqrs-user-query-service/src/integrationTest/java/dev/amir/userquery/UserQueryTest.java
+++ cqrs-user-query-service/src/integrationTest/java/dev/amir/userquery/UserQueryTest.java
@@ -1,8 +1,7 @@
 package dev.amir.userquery;
 
-import dev.amir.userquery.application.port.output.UserOutputPort;
-import dev.amir.userquery.domain.entity.User;
-import dev.amir.userquery.domain.exception.UserNotFoundException;
+import dev.amir.userquery.framework.output.sql.entity.UserJpa;
+import dev.amir.userquery.framework.output.sql.repository.UserJpaRepository;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -28,28 +27,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserQueryTest {
-
     @MockBean
-    UserOutputPort userOutputPortMock;
-
+    private UserJpaRepository userJpaRepositoryMock;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void test_GetAllUsers() throws Exception {
+    public void test_LoadContext() {
+        assertNotNull(userJpaRepositoryMock);
+        assertNotNull(mockMvc);
+    }
 
-        List<User> mockUsers = Arrays.asList(
-                new User() {{
+    @Test
+    void test_GetAllUsers() throws Exception {
+        List<UserJpa> mockUsers = Arrays.asList(
+                new UserJpa() {{
                     setId("1");
                     setName("kevin");
                 }},
-                new User() {{
+                new UserJpa() {{
                     setId("2");
                     setName("john");
                 }}
         );
 
-        when(userOutputPortMock.getAll()).thenReturn(mockUsers);
+        when(userJpaRepositoryMock.findAll()).thenReturn(mockUsers);
 
         mockMvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -58,16 +60,16 @@ public class UserQueryTest {
                 .andExpect(jsonPath("$.users[0].id").value("1"))
                 .andExpect(jsonPath("$.users[1].id").value("2"));
 
-        verify(userOutputPortMock).getAll();
+        verify(userJpaRepositoryMock).findAll();
     }
 
     @Test
     public void test_GetUsersById() throws Exception {
-        User mockUser = new User();
+        UserJpa mockUser = new UserJpa();
         String expectedUuid = UUID.randomUUID().toString();
         mockUser.setName("kevin");
         mockUser.setId(expectedUuid);
-        when(userOutputPortMock.getById(any(String.class))).thenReturn(Optional.of(mockUser));
+        when(userJpaRepositoryMock.findById(any(String.class))).thenReturn(Optional.of(mockUser));
 
         mockMvc.perform(get("/users/{userId}", expectedUuid)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -75,34 +77,31 @@ public class UserQueryTest {
                 .andExpect(jsonPath("$.user.id").value(expectedUuid))
                 .andExpect(status().isOk());
 
-        verify(userOutputPortMock).getById(eq(expectedUuid));
+        verify(userJpaRepositoryMock).findById(eq(expectedUuid));
     }
 
     @Test
     public void test_HandleUnknownException_ForGetAllUsers() throws Exception {
-        when(userOutputPortMock.getAll()).thenThrow(InternalError.class);
+        when(userJpaRepositoryMock.findAll()).thenThrow(InternalError.class);
 
         mockMvc.perform(get("/users")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isInternalServerError())
                 .andExpect(content().string("Internal error"));
 
-        verify(userOutputPortMock).getAll();
+        verify(userJpaRepositoryMock).findAll();
     }
 
     @Test
     public void test_HandleUserNotFoundException_ForGetUserById() throws Exception {
         String expectedUuid = UUID.randomUUID().toString();
-        UserNotFoundException ex = new UserNotFoundException(expectedUuid);
-
-        when(userOutputPortMock.getById(any(String.class))).thenReturn(Optional.empty());
+        when(userJpaRepositoryMock.findById(any(String.class))).thenReturn(Optional.empty());
 
         mockMvc.perform(get("/users/{userId}", expectedUuid)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("User not found"));
 
-        assertEquals("User with id:" + expectedUuid + " Not found", ex.getMessage());
-        verify(userOutputPortMock).getById(eq(expectedUuid));
+        verify(userJpaRepositoryMock).findById(eq(expectedUuid));
     }
 }
--- cqrs-user-query-service/src/main/java/dev/amir/userquery/framework/output/sql/mapper/UserEntityMapper.java
+++ cqrs-user-query-service/src/main/java/dev/amir/userquery/framework/output/sql/mapper/UserEntityMapper.java
@@ -1,14 +1,14 @@
 package dev.amir.userquery.framework.output.sql.mapper;
 
 import dev.amir.userquery.domain.entity.User;
-import dev.amir.userquery.framework.output.sql.entity.UserEntity;
+import dev.amir.userquery.framework.output.sql.entity.UserJpa;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserEntityMapper {
 
-    User convert(UserEntity userEntity);
+    User convert(UserJpa userJpa);
 
-    UserEntity convert(User user);
+    UserJpa convert(User user);
 }
