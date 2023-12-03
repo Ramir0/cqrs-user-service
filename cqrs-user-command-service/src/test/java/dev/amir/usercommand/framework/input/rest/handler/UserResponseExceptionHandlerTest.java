4f9d0c8bc226849264ee982c8d11293cd0520208
Amir Aranibar <amir.aranibar@gmail.com>
2023-12-03 18:03:58 +0100

    Rename unit tests in order to follow defined standards

--- cqrs-user-command-service/src/test/java/dev/amir/usercommand/framework/input/rest/handler/UserResponseExceptionHandlerTest.java
+++ cqrs-user-command-service/src/test/java/dev/amir/usercommand/framework/input/rest/handler/UserResponseExceptionHandlerTest.java
@@ -1,9 +1,12 @@
 package dev.amir.usercommand.framework.input.rest.handler;
 
 import dev.amir.usercommand.domain.exception.UserNotFoundException;
+import java.lang.reflect.Parameter;
 import java.util.UUID;
+import dev.amir.usercommand.util.RandomObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -19,7 +22,7 @@ class UserResponseExceptionHandlerTest {
     }
 
     @Test
-    void test_handleUnknownException() {
+    void test_handleUnknownException_ReturnsStatusCode500() {
         Exception ex = new Exception();
 
         ResponseEntity<String> response = underTest.handleUnknownException(ex);
@@ -29,7 +32,7 @@ class UserResponseExceptionHandlerTest {
     }
 
     @Test
-    void test_handleUserNotFoundException() {
+    void test_handleUserNotFoundException_ReturnsStatusCode404() {
         UserNotFoundException ex = new UserNotFoundException(UUID.randomUUID());
 
         ResponseEntity<String> response = underTest.handleUserNotFoundException(ex);
@@ -39,7 +42,7 @@ class UserResponseExceptionHandlerTest {
     }
 
     @Test
-    void test_handleBadRequestException() {
+    void test_handleBadRequestException_ReturnsStatusCode400() {
         MethodArgumentTypeMismatchException ex = new MethodArgumentTypeMismatchException(
                 "userId",
                 Long.class,
