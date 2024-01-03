96d48b2723494af868641252d99a8095b25885a5
Amir Aranibar <amir.aranibar@gmail.com>
2024-01-03 12:41:57 +0100

    Set CreatedAt and UpdatedAt for User using AOP

---
+++ cqrs-user-command-service/src/test/java/dev/amir/usercommand/application/aop/OnUserAspectTest.java
@@ -0,0 +1,51 @@
+package dev.amir.usercommand.application.aop;
+
+import dev.amir.usercommand.domain.entity.User;
+import org.aspectj.lang.JoinPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class OnUserAspectTest {
+    private OnUserAspect underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new OnUserAspect();
+    }
+
+    @Test
+    void test_SetCreatedAt() {
+        User user = new User();
+        Object[] args = {user};
+        JoinPoint joinPointMock = mock(JoinPoint.class);
+
+        when(joinPointMock.getArgs()).thenReturn(args);
+
+        underTest.setCreatedAt(joinPointMock);
+
+        assertNotNull(user.getCreatedAt());
+        assertNull(user.getUpdatedAt());
+        verify(joinPointMock).getArgs();
+    }
+
+    @Test
+    void test_SetUpdatedAt() {
+        User user = new User();
+        Object[] args = {user};
+        JoinPoint joinPointMock = mock(JoinPoint.class);
+
+        when(joinPointMock.getArgs()).thenReturn(args);
+
+        underTest.setUpdatedAt(joinPointMock);
+
+        assertNotNull(user.getUpdatedAt());
+        assertNull(user.getCreatedAt());
+        verify(joinPointMock).getArgs();
+    }
+}
