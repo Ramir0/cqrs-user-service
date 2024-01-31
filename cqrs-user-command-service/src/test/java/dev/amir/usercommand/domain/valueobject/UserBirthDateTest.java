9fcc5c0279dc2c13b017e9259070ddcf39cc61c9
Kevin Garcia <huascraft@hotmail.com>
2024-01-31 10:05:47 -0400

    fix coverage

--- cqrs-user-command-service/src/test/java/dev/amir/usercommand/domain/valueobject/UserBirthDateTest.java
+++ cqrs-user-command-service/src/test/java/dev/amir/usercommand/domain/valueobject/UserBirthDateTest.java
@@ -2,9 +2,12 @@ package dev.amir.usercommand.domain.valueobject;
 
 import dev.amir.usercommand.util.RandomObject;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserBirthDateTest {
 
@@ -43,5 +46,35 @@ public class UserBirthDateTest {
         UserBirthDate birthDate2 = new UserBirthDate(expected);
 
         assertEquals(birthDate1, birthDate2);
+    }
+
+    @Test
+    void test_ValueWithIncorrectFormat_ReturnsDateTimeParseException() {
+        assertAll(
+                () -> assertThrows(DateTimeParseException.class,
+                        () -> new UserBirthDate(LocalDate.parse("01-05-1900"))),
+                () -> assertThrows(DateTimeParseException.class,
+                        () -> new UserBirthDate(LocalDate.parse("2000/10/01")))
+        );
+    }
+
+    @Test
+    void test_ValueWithSpaces_ReturnsDateTimeParseException() {
+        assertAll(
+                () -> assertThrows(DateTimeParseException.class,
+                        () -> new UserBirthDate(LocalDate.parse(" 2000-05-06 "))),
+                () -> assertThrows(DateTimeParseException.class,
+                        () -> new UserBirthDate(LocalDate.parse(" ")))
+        );
+    }
+
+    @Test
+    void test_ValueWithText_ReturnsDateTimeParseException() {
+        DateTimeParseException exception = assertThrows(
+                DateTimeParseException.class, () -> {
+                    new UserBirthDate(LocalDate.parse("abc"));
+                });
+
+        assertEquals(DateTimeParseException.class, exception.getClass());
     }
 }
